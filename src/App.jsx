d960836860c3ca370d5cec4f20147039a0a9d057
Smeet Ghori <72125852+SMITGHORI@users.noreply.github.com>
2024-02-05 16:51:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Background from "./component/Background";
+import Foreground from "./component/Foreground";
+import Card from "./component/Card";
+
+function App() {
+  return (
+    <div className="relative w-full h-screen bg-zinc-800">
+      <Background />
+      <Foreground />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
